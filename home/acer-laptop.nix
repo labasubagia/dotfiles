32623c9ab6d03f209464a6e2dc5b881e56b9abdb
Laba Subagia <labasubagia22@gmail.com>
2023-05-10 07:40:30 +0800

    add acer-laptop themes & icons

--- home/acer-laptop.nix
+++ home/acer-laptop.nix
@@ -9,4 +9,16 @@
     # Currently using ansible for create & config ssh
     # ./config/ssh.nix
   ];
+
+  home.packages = with pkgs; [
+
+    # themes
+    ant-theme
+    plata-theme
+
+    # icons
+    arc-icon-theme
+    colloid-icon-theme
+    tela-icon-theme
+  ];
 }
