4bc57d0f920c0813d4b0552ec69958c63e17278c
Laba Subagia <labasubagia22@gmail.com>
2023-11-19 00:32:14 +0800

    chore: update gui app

--- home/gui/default.nix
+++ home/gui/default.nix
@@ -4,6 +4,12 @@
   home.packages = with pkgs; [
     brave
     vscode
-    postman
+
+    # postman has usr problem
+    # postman
+
+    # redis insight has problem with nodejs ssl
+    # try tomorrow, if fail make an issue
+    # redisinsight
   ];
 }
