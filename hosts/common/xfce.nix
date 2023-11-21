f4d8d4f503b96745c47fbc1be5a51d3b4cb47a11
Laba Subagia <labasubagia22@gmail.com>
2023-11-21 19:21:23 +0800

    chore: rm picom

--- hosts/common/xfce.nix
+++ hosts/common/xfce.nix
@@ -10,12 +10,4 @@
     xfce.xfce4-whiskermenu-plugin
     xfce.xfce4-pulseaudio-plugin
   ];
-
-  services.picom = {
-    enable = true;
-    fade = true;
-    inactiveOpacity = 0.9;
-    shadow = true;
-    fadeDelta = 4;
-  };
 }
