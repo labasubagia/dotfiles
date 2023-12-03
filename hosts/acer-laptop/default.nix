e571b70801fb9e663d34efa0af8ceb7eb64f0cf1
Laba Subagia <labasubagia22@gmail.com>
2023-12-03 22:51:00 +0800

    chore: audio

--- hosts/acer-laptop/default.nix
+++ hosts/acer-laptop/default.nix
@@ -58,7 +58,7 @@
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -66,7 +66,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -80,7 +80,7 @@
   users.users.user = {
     isNormalUser = true;
     description = "user";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    extraGroups = [ "audio" "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [ ];
   };
 
