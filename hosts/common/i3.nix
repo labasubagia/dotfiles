7bc16e05441a881472b72513ae815e0f72e6167b
Laba Subagia <labasubagia22@gmail.com>
2023-12-03 18:28:50 +0800

    chore: fix i3

--- hosts/common/i3.nix
+++ hosts/common/i3.nix
@@ -1,24 +1,31 @@
 { pkgs, ... }:
 
 {
+
+  environment.pathsToLink = [ "/libexec" ];
+
   services.xserver = {
     enable = true;
+
+    desktopManager = {
+      xterm.enable = true;
+    };
+
+    displayManager = {
+      defaultSession = "none+i3";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+        i3lock
+        i3blocks
+        lxappearance
+      ];
+    };
   };
 
   programs.dconf.enable = true;
-
-  desktopManager = {
-    xterm.enable = true;
-  };
-
-  windowManager.i3 = {
-    defaultSession = "none+i3";
-    extraPackages = with pkgs; [
-      dmenu
-      i3status
-      i3lock
-      i3blocks
-      lxappearance
-    ];
-  };
 }
