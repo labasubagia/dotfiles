e2ccbead98d13540dc2873395d1a4e7ab5fb5b2b
Laba Subagia <labasubagia22@gmail.com>
2023-12-04 00:33:11 +0800

    chore: i3status modules

--- home/common/desktops/i3/default.nix
+++ home/common/desktops/i3/default.nix
@@ -31,6 +31,62 @@
     general = {
       interval = 1;
     };
+    modules = {
+      "wireless _first_" = {
+        position = 1;
+        settings = {
+          format_up = "W: (%quality at %essid) %ip";
+          format_down = "W: down";
+        };
+      };
+      "ethernet _first_" = {
+        position = 2;
+        settings = {
+          format_up = "E: %ip (%speed)";
+          format_down = "E: down";
+        };
+      };
+      "battery all" = {
+        position = 3;
+        settings = {
+          format = "%status %percentage %remaining";
+        };
+      };
+      "disk /" = {
+        position = 4;
+        settings = {
+          format = "/ %avail";
+        };
+      };
+      "load" = {
+        position = 5;
+        settings = {
+          format = "%1min";
+        };
+      };
+      "memory" = {
+        position = 6;
+        settings = {
+          format = "%used | %available";
+          threshold_degraded = "1G";
+          format_degraded = "MEMORY < %available";
+        };
+      };
+      "volume master" = {
+        position = 7;
+        settings = {
+          format = "♪: %volume";
+          format_muted = "♪: muted (%volume)";
+          device = "pulse:1";
+        };
+      };
+      "tztime local" = {
+        position = 8;
+        settings = {
+          format = "%Y-%m-%d %H:%M:%S";
+        };
+      };
+    };
   };
 
   services.picom = {
