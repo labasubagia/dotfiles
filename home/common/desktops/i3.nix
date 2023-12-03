534b772caabcabd35930c7a8ac798f6c36928d3d
Laba Subagia <labasubagia22@gmail.com>
2023-12-03 20:58:12 +0800

    chore: i3 add picom

--- home/common/desktops/i3.nix
+++ home/common/desktops/i3.nix
@@ -2,9 +2,29 @@
 
 {
   xsession.windowManager.i3 = {
+    enable = true;
     config = {
-      gaps = "5px";
+      modifier = "Mod4";
+      gaps = {
+        inner = 5;
+      };
       terminal = "alacritty";
+    };
+  };
+
+  services.picom = {
+    enable = true;
+    settings = {
+      inactive-opacity = 0.8;
+      active-opacity = 0.8;
+      frame-opacity = 0.8;
+      blur = {
+        method = "kawase";
+        strength = 9;
+        background = false;
+        background-frame = false;
+        background-fixed = false;
+      };
     };
   };
 }
