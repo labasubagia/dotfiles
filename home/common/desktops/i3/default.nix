6ee81a22e7b93cf28aa9fa73f46d5afba3c09ce9
Laba Subagia <labasubagia22@gmail.com>
2023-12-06 07:23:47 +0800

    chore: update i3

--- home/common/desktops/i3/default.nix
+++ home/common/desktops/i3/default.nix
@@ -43,12 +43,12 @@ in
         size = 9.0;
       };
       colors = {
-        focused = { };
-        focusedIncactive = { };
-        unfocused = { };
-        urgent = { };
-        placeholder = { };
-        background = { };
+        # focused = { };
+        # focusedInactive = { };
+        # unfocused = { };
+        # urgent = { };
+        # placeholder = { };
+        # background = { };
       };
       modifier = "Mod4";
       gaps = {
@@ -89,6 +89,39 @@ in
     extraConfig = "
       default_border pixel 3
       default_floating_border pixel 3
+
+      set $rosewater #f5e0dc
+      set $flamingo  #f2cdcd
+      set $pink      #f5c2e7
+      set $mauve     #cba6f7
+      set $red       #f38ba8
+      set $maroon    #eba0ac
+      set $peach     #fab387
+      set $green     #a6e3a1
+      set $teal      #94e2d5
+      set $sky       #89dceb
+      set $sapphire  #74c7ec
+      set $blue      #89b4fa
+      set $lavender  #b4befe
+      set $text      #cdd6f4
+      set $subtext1  #bac2de
+      set $subtext0  #a6adc8
+      set $overlay2  #9399b2
+      set $overlay1  #7f849c
+      set $overlay0  #6c7086
+      set $surface2  #585b70
+      set $surface1  #45475a
+      set $surface0  #313244
+      set $base      #1e1e2e
+      set $mantle    #181825
+      set $crust     #11111b
+
+      client.focused           $lavender $base $text  $rosewater $lavender
+      client.focused_inactive  $overlay0 $base $text  $rosewater $overlay0
+      client.unfocused         $overlay0 $base $text  $rosewater $overlay0
+      client.urgent            $peach    $base $peach $overlay0  $peach
+      client.placeholder       $overlay0 $base $text  $overlay0  $overlay0
+      client.background        $base
     ";
   };
 
@@ -106,9 +139,9 @@ in
     enable = true;
     settings = {
       backend = "glx";
-      inactive-opacity = 0.8;
       active-opacity = 0.8;
       frame-opacity = 0.8;
+      inactive-opacity = 0.7;
       blur = {
         method = "dual_kawase";
         strength = 9;
