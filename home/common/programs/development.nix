f336c1c528fd5770f4561f3c4f07df4da343f464
Laba Subagia <labasubagia22@gmail.com>
2023-12-06 13:41:51 +0800

    chore: update development tools

--- home/common/programs/development.nix
+++ home/common/programs/development.nix
@@ -1,14 +1,44 @@
-{ pkgs, ... }:
+{ pkgs, stdenv, lib, ... }:
 
 {
   home.packages = with pkgs; [
+    # lsp
     rnix-lsp
+
+    # git hooks
     pre-commit
 
+    # build
     gcc
+    gnumake
+
     go
     rustup
     nodejs
     nodePackages.pnpm
   ];
+
+  # # asdf alternative
+  # # https://github.com/jdx/rtx
+  # # ! Don't use it, still have problem with several package e.g. nodejs
+  # programs.rtx = {
+  #   enable = true;
+
+  #   settings = {
+  #     env = {
+  #       RTX_NODE_FORCE_COMPILE = "1";
+  #     };
+
+  #     tools = {
+  #       node = "lts";
+  #       pnpm = "latest";
+  #       go = "latest";
+  #     };
+
+  #     settings = {
+  #       verbose = true;
+  #       experimental = false;
+  #     };
+  #   };
+  # };
 }
