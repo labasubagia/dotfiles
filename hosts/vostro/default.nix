cba4e1c4710e634b1de2378f0aae3a74be9bc98c
Laba Subagia <labasubagia22@gmail.com>
2024-06-16 12:10:06 +0800

    chore: vostro use xfce

--- hosts/vostro/default.nix
+++ hosts/vostro/default.nix
@@ -10,7 +10,7 @@
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../common
-      ../common/pantheon.nix
+      ../common/xfce.nix
     ];
 
   # Bootloader.
