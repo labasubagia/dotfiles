3dcc2ac060252dd67730ef6004140f3075a4b983
Laba Subagia <labasubagia22@gmail.com>
2024-06-21 04:32:14 +0800

    chore: ssh

--- home/common/program/shell/ssh.nix
+++ home/common/program/shell/ssh.nix
@@ -1,10 +1,13 @@
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
 
 let
   sshKeys = [
     { name = "github"; host = "github.com"; }
     { name = "gitlab"; host = "gitlab.com"; }
+
+    # example one key for multiple host
     { name = "phabricator"; host = "phabricator.sirclo.com"; }
+    { name = "phabricator"; host = "phabricator.other-host.com"; }
   ];
 in
 {
@@ -12,23 +15,25 @@ in
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
     defaultSopsFile = ../../../secrets.yaml;
     secrets = builtins.listToAttrs (
-      map
-        (key: {
-          name = "ssh_keys/${key.name}/private";
-          value = {
-            path = "${config.home.homeDirectory}/.ssh/id_rsa_${key.name}";
-          };
-        })
-        sshKeys
-      ++
-      map
-        (key: {
-          name = "ssh_keys/${key.name}/public";
-          value = {
-            path = "${config.home.homeDirectory}/.ssh/id_rsa_${key.name}.pub";
-          };
-        })
-        sshKeys
+      lib.lists.unique (
+        map
+          (key: {
+            name = "ssh_keys/${key.name}/private";
+            value = {
+              path = "${config.home.homeDirectory}/.ssh/id_rsa_${key.name}";
+            };
+          })
+          sshKeys
+        ++
+        map
+          (key: {
+            name = "ssh_keys/${key.name}/public";
+            value = {
+              path = "${config.home.homeDirectory}/.ssh/id_rsa_${key.name}.pub";
+            };
+          })
+          sshKeys
+      )
     );
   };
 
