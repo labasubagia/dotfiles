d79c6e882ded86537b15f6e1bbc1b720a615d663
Laba Subagia <labasubagia22@gmail.com>
2024-07-01 10:30:21 +0800

    chore: add filezilla

--- home/common/program/ui/default.nix
+++ home/common/program/ui/default.nix
@@ -9,6 +9,7 @@
   home.packages = with pkgs; [
     brave
     libreoffice
+    filezilla
 
     # NOTE: proprietary software sometimes has problem with nix packages, prefer use flatpak
     # postman
